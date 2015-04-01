e8abc31a0fa7d7b25afb9a77853c73185a6fd415
Jorgezar <zarembajorge@wp.pl>
2015-04-01 18:29:07 +0200

    koment

--- neuroroll.py
+++ neuroroll.py
@@ -3,33 +3,64 @@ from tools import *
 
 class NeuroRoll():
     def __init__(self):
+        self.roll = roll_3d20()
         self.attr = 12
-        self.dificulty = 0
+        
         self.thresholds = [2, 0, -2, -5, -8, -11, -15]
+        self.threshold_marker = 1
+        self.dificulty = self.thresholds[self.threshold_marker]
         self.skill = 10
         self.succeses = 0
+    
+    
+    
     def set_threshold(self, argument):
         if argument == 'l':
-            self.dificulty = self.thresholds[0]
+            self.threshold.marker = 0
         elif argument == 'n':
-            self.dificulty = self.thresholds[1]
+            self.threshold.marker = 1
         elif argument == 'p':
-            self.dificulty = self.thresholds[2]
+            self.threshold.marker = 2
         elif argument == 't':
-            self.dificulty = self.thresholds[3]
+            self.threshold.marker = 3
         elif argument == 'b':
-            self.dificulty = self.thresholds[4]
+            self.threshold.marker = 4
         elif argument == 'c':
-            self.dificulty = self.thresholds[5]
+            self.threshold.marker = 5
         elif argument == 'f':
-            self.dificulty = self.thresholds[6]
+            self.threshold.marker = 6
         else: 
-            self.dificulty = 0
+            self.threshold_marker = 1
         print "dificulty set to:", self.dificulty
         #modifiers = [2, 0, -2, -5, -8, -11, -15]
-        
     
+    def threshold_check(self):
+        for roll in self.roll:
+            if roll == 1:
+                print u'jedynka! suwak w dół, kolego!'
+                self.threshold_marker -=1
+            elif roll == 20:
+                print u'dwudziestka! trudność rośnie'
+                self.threshold_marker +=1 
+            else: pass
+        if self.skill == 0:
+            self.threshold_marker +=1
+            print u'brakuje ci skilla, trudność rośnie'
+        elif self.skill >= 4:
+            print u'suwak!'
+            self.threshold_marker -= (self.skill / 4)
+        else: pass
+        
+        
     def skill_check(self):
+        if self.threshold_marker < 0:
+           print u'automatyczny sukces'
+           self.succeses = 3
+           
+        elif self.threshold_marker > 6:
+           print u'automatyczna porażka'
+           
+        else: pass
         dice_roll = sorted(roll_3d20())
         print u'twój rzut:', dice_roll
         print 'testowana cecha', (self.attr + self.dificulty)
@@ -45,7 +76,6 @@ class NeuroRoll():
                 print roll, 'failure'
         for failed_roll in sorted(failure_dice):
             failure_margin = failed_roll - (self.attr + self.dificulty)
-            
             print u'wyrzuciłem %r, do sukcesu zabrakło %r.' %(failed_roll, failure_margin)
             if failure_margin <= self.skill:
                 self.skill -= failure_margin
@@ -64,4 +94,5 @@ class NeuroRoll():
                 
 myroll = NeuroRoll()
 myroll.set_threshold('t')
+myroll.threshold_check()
 myroll.skill_check()
